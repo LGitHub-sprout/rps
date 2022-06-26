714b58993b59a6a955c880306f0482826d728b27
LesterC <lsysworks@gmail.com>
2022-06-26 14:08:30 -0400

    Finsih 2b. RPS-UI: Add e.Listener, Log Btn Content
    - js/rps.js
      * Add eventListener to human player buttons.
        - Select all player buttons.
        - Loop through buttons, add e.Listener and call playRound func.
        - Log button selection textContent on button click.

--- rps.js
+++ rps.js
@@ -1,6 +1,74 @@
 /**************************** Rock Paper Scissors App ***
   https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors#assignment
+
+  https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors#assignment 
+  2a.
+  2b.
+    Create three buttons, one for each selection. 
+    Add an event listener to the buttons that call(s) your playRound function (and returns) the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+    
+  Errors/Notes:
+})
 */
+const rpsBtns = document.querySelectorAll('.button-wrap button');
+rpsBtns.forEach((button) => {
+  button.addEventListener('click', playRound);
+});
+function playRound(e) {
+  humanSelection = e.target.textContent;
+  console.log(humanSelection)
+  // humanSelection = humanSelection.toLowerCase();
+// Is it possible to convert to an array (or is object better?) and loop through results?
+  switch (true) {
+    case humanSelection === 'rock' && computerSelection === 'rock':
+      console.log('It\'s a tie.');
+      break;
+    case humanSelection === 'rock' && computerSelection === 'paper':
+      console.log(`${computerSelection} SMOTHERS ${humanSelection}`);
+      computerScore++;
+      break;
+    case humanSelection === 'rock' && computerSelection === 'scissors':
+      console.log(`${humanSelection} CRUSHES ${computerSelection}`);
+      humanScore++;
+      break;
+    
+    case humanSelection === 'paper' && computerSelection === 'paper':
+      console.log('It\'s a tie.');
+      break;
+    case humanSelection === 'paper' && computerSelection === 'scissors':
+      console.log(`${computerSelection} SLICES ${humanSelection}`);
+      computerScore++;
+      break;
+    case humanSelection === 'paper' && computerSelection === 'rock':
+      console.log(`${humanSelection} SMOTHERS ${computerSelection}`);
+      humanScore++;
+      break;
+    
+    case humanSelection === 'scissors' && computerSelection === 'scissors':
+      console.log('It\'s a tie.');
+      break;
+    case humanSelection === 'scissors' && computerSelection === 'rock':
+      console.log(`${computerSelection} CRUSHES ${humanSelection}`);
+      computerScore++;
+      break;
+    case humanSelection === 'scissors' && computerSelection === 'paper':
+      console.log(`${humanSelection} SLICES ${computerSelection}`);
+      humanScore++;
+      break;
+  }
+
+//   if (humanScore === 5) {
+//     console.log('HUMANITY WINS!');
+//   } else if (computerScore === 5) {
+//     console.log('Submit to the superior A.I. Race')
+//   }
+
+//   console.log(`humanScore: ${humanScore}`);
+//   console.log(`computerScore: ${computerScore}`);
+
+// return `${humanSelection} and ${computerSelection}`;
+} // playRound
+
 function computerPlay(randoNum) {
   randoNum = parseInt(Math.random() * (100 - 1) + 7);
   if (randoNum > 100) {
@@ -22,11 +90,12 @@ function computerPlay(randoNum) {
 function playGame() {
   let computerScore = 0;
   let humanScore = 0;
+
   // you want statement to return false as soon as one of your conditions turns false
   // you want your loop to run as long as both conditions are true '@riri's babybump'
   // && returns TRUE if BOTH operands are TRUTHY and FALSE otherwise.
-  for (let i = 1; humanScore < 5 && computerScore < 5; i++) { 
-    const computerSelection = computerPlay();
+  // for (let i = 1; humanScore < 5 && computerScore < 5; i++) { 
+  //   const computerSelection = computerPlay();
 
 // console.log('this is computerSelection: ', computerSelection);
     let humanSelection = prompt('Make a selection: Rock, Paper, or Scissors');
@@ -88,5 +157,5 @@ function playGame() {
     // playRound(humanSelection, computerSelection);
     // console.log(`This is humanSelection ${humanSelection}`);
     // console.log(`This is computerSelection ${computerSelection}`);
-  } // for loop
+  // } // for loop
 } // playGame
