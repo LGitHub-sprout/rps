0ff2d8972b07252f4262a4c59950505de594bde8
LesterC <lsysworks@gmail.com>
2022-06-30 14:03:10 -0400

    Final 2d,e,f RPS-UI: Refactor; DOM; HTML Bug Fixes
    - js/rps.js
      * Move score vars into global context.
      * Use textContent property to output running scores.
      * Add alert to announe winner, refresh screen for new game.
      * Clean code.
    
    - odin-project/rps.html
      * Bug fixes.
      * Add scores markup.
    
    - styles/odin-project-styles/odin.css
      * Initial commit: final RPS-UI version.

--- rps.js
+++ rps.js
@@ -4,52 +4,92 @@
   https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors#assignment 
   2a.
   2b.
-    Create three buttons, one for each selection. 
-    Add an event listener to the buttons that call(s) your playRound function (and returns) the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
-    
-  Errors/Notes:
-})
+  2c. Display the running score, and announce a winner of the game once one player reaches 5 points.
+      This seems to be the trickiest bit of this challenge.
+      What's happening currently:
+        Scoring isn't updated properly: I get an initial point on the first round but subsequent rounds write over previous scores.
+          Using playGame2 callback function w a loop: seems like I'd be better off just rewriting the entire app.
+          Everything breaks.
+      Pseudocode:
+        User clicks one of three choices: rock paper scissors
+        Compare user choice of rock paper scissors to computer's random choice.
+        Depending which choice wins:
+          Optional: display results of each round to the browser.
+          Output the running score to the browser and
+          announce a winner of the game when one player reaches 5 points.
+
+      Tues 9:30am
+        going back to EOD Monday. it's basically working except score-keeping.
+        I can't pull answer from thin air; I have no experience, so I googled this:
+        https://dev.to/davidchedrick/creating-a-simple-timer-and-score-keeper-in-javascript-394g
+        a.  Moving score initializations to global scope since maybe being overwritten is caused by local scope?
+        b.  This seems to have solved the problem... I'm hoping ??
+
+      Bugs:
+        1.  Sometimes I get a number like 81 or 85 instead of a selection.
+            Guessing computerPlay() being called early?
+            No. This broke the app.
+        2.  Counter isn't going to 5.
+            Fixed.
+        3.  First round isn't being counted.
+              Initializing scores at zero works for logging but not DOM.
+              Fixed.
 */
 const rpsBtns = document.querySelectorAll('.button-wrap button');
 rpsBtns.forEach((button) => {
   button.addEventListener('click', playRound);
 });
+
+let computerScore = 1;
+let humanScore = 1;
+
+const rpsScore = document.querySelector('.rps-score');
+const computerScoreSpan = document.createElement('span');
+const humanScoreSpan = document.createElement('span');
+computerScoreSpan.textContent = 'Computer: 0';
+humanScoreSpan.textContent =  'Human: 0';
+rpsScore.append(computerScoreSpan, humanScoreSpan);
+
 function playRound(e) {
 
-  let computerScore = 0;
-  let humanScore = 0;
-
   const computerSelection = computerPlay();
-  humanSelection = e.target.textContent.toLowerCase();
-  const singleRoundResult = document.querySelector('.playRoundResult');
 
-  console.log(computerSelection)
-  // Is it possible to convert to an array (or is object better?) and loop through results?
-  // Would I even need an array?
+  const humanSelection = e.target.textContent.toLowerCase();
+  const singleRoundResult = document.querySelector('.play-round-result');
+
+  console.log(computerSelection);
+  // Is it possible to use an array (or is object better?) and loop through results?
+  // See https://github.com/rlmoser99/rock-paper-scissors/blob/master/script.js for combinging if stmts
   switch (true) {
     case humanSelection === 'rock' && computerSelection === 'rock':
-      const selectedBtn = document.querySelector('.button-wrap button');
-      // selectedBtn.style.cssText = 'background-color: #a41b5f;color: linen;border: solid #a41b5f 1px;border-radius:3px';
       singleRoundResult.textContent = 'It\'s a tie.';
       break;
     case humanSelection === 'rock' && computerSelection === 'paper':
       singleRoundResult.textContent = `${computerSelection} SMOTHERS ${humanSelection}`;
+      computerScoreSpan.textContent = 'Computer: ';
+      computerScoreSpan.append(computerScore);
       computerScore++;
       break;
     case humanSelection === 'rock' && computerSelection === 'scissors':
-        singleRoundResult.textContent = `${humanSelection} CRUSHES ${computerSelection}`;
+      singleRoundResult.textContent = `${humanSelection} CRUSHES ${computerSelection}`;
+      humanScoreSpan.textContent = 'Human: ';
+      humanScoreSpan.append(humanScore);
       humanScore++;
       break;
-      
+
     case humanSelection === 'paper' && computerSelection === 'paper':
       singleRoundResult.textContent = `It\'s a tie.`;
       break;
     case humanSelection === 'paper' && computerSelection === 'scissors':
       singleRoundResult.textContent = `${computerSelection} SLICE ${humanSelection}`;
+      computerScoreSpan.textContent = 'Computer: ';
+      computerScoreSpan.append(computerScore);
       computerScore++;
       break;
     case humanSelection === 'paper' && computerSelection === 'rock':
       singleRoundResult.textContent = `${humanSelection} SMOTHERS ${computerSelection}`;
+      humanScoreSpan.textContent = 'Human: ';
+      humanScoreSpan.append(humanScore);
       humanScore++;
       break;
     
@@ -58,116 +98,45 @@ function playRound(e) {
       break;
     case humanSelection === 'scissors' && computerSelection === 'rock':
       singleRoundResult.textContent = `${computerSelection} CRUSHES ${humanSelection}`;
-      // console.log(`${computerSelection} CRUSHES ${humanSelection}`);
+      computerScoreSpan.textContent = 'Computer: ';
+      computerScoreSpan.append(computerScore);
       computerScore++;
       break;
     case humanSelection === 'scissors' && computerSelection === 'paper':
       singleRoundResult.textContent = `${humanSelection} SLICE ${computerSelection}`;
       // console.log(`${humanSelection} SLICES ${computerSelection}`);
+      humanScoreSpan.textContent = 'Human: ';
+      humanScoreSpan.append(humanScore);
       humanScore++;
       break;
   }
 
-//   if (humanScore === 5) {
-//     console.log('HUMANITY WINS!');
-//   } else if (computerScore === 5) {
-//     console.log('Submit to the superior A.I. Race')
-//   }
-
-//   console.log(`humanScore: ${humanScore}`);
-//   console.log(`computerScore: ${computerScore}`);
-
-// return `${humanSelection} and ${computerSelection}`;
-  return humanSelection;
+  if (humanScore === 6) {
+    alert('Humanity WINS!');
+    // https://stackoverflow.com/questions/29884654/button-that-refreshes-the-page-on-click
+    window.location.reload(true);
+  } else if (computerScore === 6) {
+    alert('Submit to Superior Artificial Intelligence!');
+    window.location.reload(true);
+  }
 } // playRound
 
+// https://css-tricks.com/lots-of-ways-to-use-math-random-in-javascript/#aa-how-do-you-handle-repeated-values
 function computerPlay(randoNum) {
-  randoNum = parseInt(Math.random() * (100 - 1) + 7);
-  if (randoNum > 100) {
-    return randoNum - 20;
-  } else if (randoNum < 1) {
-    return randoNum + Math.random() * (100 - 1);
-  }
+  randoNum = (Math.random() * (100) + 1);
   if (randoNum >= 1 && randoNum < 33) {
-    return randoNum = 'rock';
+    return 'rock';
   } else if (randoNum >= 34 && randoNum <= 66) {
-    return randoNum = 'scissors';
+    return 'scissors';
   } else if (randoNum >= 67 && randoNum <= 100) {
-    return randoNum = 'paper';
+    return 'paper';
   }
 }
 // computerPlay();
 
-// playGame();
-function playGame() {
-  let computerScore = 0;
-  let humanScore = 0;
-
-  // you want statement to return false as soon as one of your conditions turns false
-  // you want your loop to run as long as both conditions are true '@riri's babybump'
-  // && returns TRUE if BOTH operands are TRUTHY and FALSE otherwise.
-  // for (let i = 1; humanScore < 5 && computerScore < 5; i++) { 
-  //   const computerSelection = computerPlay();
-
-// console.log('this is computerSelection: ', computerSelection);
-    let humanSelection = prompt('Make a selection: Rock, Paper, or Scissors');
-
-    playRound(humanSelection, computerSelection); 
-    
-    function playRound(humanSelection, computerSelection) {
-      humanSelection = humanSelection.toLowerCase();
-  // Is it possible to convert to an array (or is object better?) and loop through results?
-      switch (true) {
-        case humanSelection === 'rock' && computerSelection === 'rock':
-          console.log('It\'s a tie.');
-          break;
-        case humanSelection === 'rock' && computerSelection === 'paper':
-          console.log(`${computerSelection} SMOTHERS ${humanSelection}`);
-          computerScore++;
-          break;
-        case humanSelection === 'rock' && computerSelection === 'scissors':
-          console.log(`${humanSelection} CRUSHES ${computerSelection}`);
-          humanScore++;
-          break;
-        
-        case humanSelection === 'paper' && computerSelection === 'paper':
-          console.log('It\'s a tie.');
-          break;
-        case humanSelection === 'paper' && computerSelection === 'scissors':
-          console.log(`${computerSelection} SLICES ${humanSelection}`);
-          computerScore++;
-          break;
-        case humanSelection === 'paper' && computerSelection === 'rock':
-          console.log(`${humanSelection} SMOTHERS ${computerSelection}`);
-          humanScore++;
-          break;
-        
-        case humanSelection === 'scissors' && computerSelection === 'scissors':
-          console.log('It\'s a tie.');
-          break;
-        case humanSelection === 'scissors' && computerSelection === 'rock':
-          console.log(`${computerSelection} CRUSHES ${humanSelection}`);
-          computerScore++;
-          break;
-        case humanSelection === 'scissors' && computerSelection === 'paper':
-          console.log(`${humanSelection} SLICES ${computerSelection}`);
-          humanScore++;
-          break;
-      }
-
-      if (humanScore === 5) {
-        console.log('HUMANITY WINS!');
-      } else if (computerScore === 5) {
-        console.log('Submit to the superior A.I. Race')
-      }
-
-      console.log(`humanScore: ${humanScore}`);
-      console.log(`computerScore: ${computerScore}`);
-
-  return `${humanSelection} and ${computerSelection}`;
-    } // playRound
-    // playRound(humanSelection, computerSelection);
-    // console.log(`This is humanSelection ${humanSelection}`);
-    // console.log(`This is computerSelection ${computerSelection}`);
-  // } // for loop
-} // playGame
+// https://github.com/paulooms/odin-rps/blob/main/script.js
+// function computer() {
+//   let choices = ['rock', 'paper', 'scissors'];
+//   console.log(choices[Math.floor(Math.random() * choices.length)]);
+// }
+// computer(); // randomly selects one of 3 choices array
